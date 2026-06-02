118842c14b1867a7762be721c1f380f05c9ffc7e
satya rohith <msatyarohith@gmail.com>
2026-06-02 08:31:45 +0530

    build: force vercel to bundle pg and pg-hstore

--- api/index.js
+++ api/index.js
@@ -1,3 +1,7 @@
+// Force Vercel to bundle pg and pg-hstore for Sequelize
+require('pg');
+require('pg-hstore');
+
 const app = require('../backend/src/server');
 
 // Vercel serverless handler - just export the Express app
